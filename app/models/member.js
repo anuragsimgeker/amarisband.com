e1313bf7915359c4b0a9283339a62d3e2e63791c
Anurag Simgeker <anuragsimgeker@gmail.com>
2014-09-05 14:25:11 -0700

    Adding spotify music

--- app/models/member.js
+++ app/models/member.js
@@ -1,6 +1,15 @@
 import DS from 'ember-data';
 
-var Member = DS.Model.extend({});
+var attr = DS.attr;
 
 export
-default Member;
+default DS.Model.extend({
+    firstName: attr('string'),
+    lastName: attr('string'),
+    photoUrl: attr('string'),
+    instrument: attr('string'),
+    bio: attr('string'),
+    pastMember: attr('boolean')
+});
+
+// id,cover,description,start_time,end_time,is_date_only,location,name,ticket_uri,timezone,updated_time,venue
--- app/routes/members.js
+++ app/routes/members.js
@@ -23,7 +23,7 @@ default BaseRoute.extend({
             lastName: 'Staniotes',
             photoUrl: 'assets/images/derek.jpg',
             instrument: 'Guitars',
-            bio: ''
+            bio: 'A guitarist who is expanding his musical realm by playing in this new challenging genre. He has many influences of rock, blues and metal that have fueled him throughout the years. Such guitar players as Wylde, Vaughan, and Satriani are just a few to name that have inspired him to play in past hard rock and metal bands in the Bay Area. Having the opportunity to perform pieces from the US and Europe\'s most well known Symphonic Metal acts, the honor and excitement has been prodigious. He is looking forward to the band\'s future and to get to share his creative energy towards their music.'
         }, {
             id: 4,
             firstName: 'Drew',
@@ -37,7 +37,7 @@ default BaseRoute.extend({
             lastName: 'Pike',
             photoUrl: 'assets/images/joe.jpg',
             instrument: 'Drums',
-            bio: ''
+            bio: 'In 2012, relocated to the San Francisco Bay area, with a new middle school teaching position, while bringing to the Bay a love for a new genre of music, symphonic metal. The passion and love for the dichotomy of beautiful orchestral sounds with metal backing, led him to form a band that would reflect his passion, while introducing this little – known genre of music to the San Francisco Bay region and beyond.'
         }, {
             id: 6,
             firstName: 'Judy',
@@ -51,13 +51,14 @@ default BaseRoute.extend({
             lastName: 'Jiminez',
             photoUrl: 'assets/images/suzi.jpg',
             instrument: 'Vocals',
-            bio: ''
+            bio: 'By day - a music director & voice teacher, and by night - Rockin\' out with her Amaris bandmates!  Nothing is more thrilling for Susie Jimenez than performing, whether it be with professional opera companies, touring nationally with her pop/opera ensemble Bella Sorella, or this new path of singing with Amaris.  Susie joined Amaris in Spring of 2013, and is looking forward to the opportunity to work on original music with the band, and to utilize all of her vocal range & vocal abilities for this exciting genre of Symphonic Metal.'
         }, {
             id: 8,
             firstName: 'Shari',
             lastName: 'Derkson',
             photoUrl: 'assets/images/shari.jpg',
             instrument: 'Vocals',
+            pastMember: true,
             bio: ''
         }];
     }
